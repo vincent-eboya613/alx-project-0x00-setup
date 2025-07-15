84142a214e78b790b1726c825baf68a8654c4ad4
vincent-eboya613 <vincentkidavi@gmail.com>
2025-07-15 22:14:19 +0300

    Create landing.tsx

---
+++ alx-project-0x00/pages/landing.tsx
@@ -0,0 +1,13 @@
+import Button from "@/components/Button";
+import Card from "@/components/Card";
+
+const Landing: React.FC = () => {
+  return (
+    <div>
+      <h1 className=" text-xl font-extralight">Landing Page</h1>
+      <Card />
+      <Button size="medium" shape="rounded-md" />
+    </div>
+  );
+};
+export default Landing;
